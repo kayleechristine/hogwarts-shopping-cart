0159f02230c17a3c1137eabce30f5b0861c2182e
Kaylee Morris <kaylee.christine.morris@gmail.com>
2026-03-01 20:44:13 -0600

    add ref reactivity

--- src/App.vue
+++ src/App.vue
@@ -174,7 +174,7 @@
 import { ref } from 'vue'
 
 let username = "Harry"
-let shoppingCartItems = [
+let shoppingCartItems = ref([
   {
     id: 1,
     productName: 'Dragon Liver',
@@ -215,7 +215,7 @@ let shoppingCartItems = [
     quantity: 1,
     image: 'src/assets/img/Nimbus2000.jpg'
   }
-]
+])
 
 let hideDetails = ref(true)
 </script>
